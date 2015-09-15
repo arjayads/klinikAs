d14f8a7ea65a495612e1033a68fc6dcd6c344510
Arjay Ads <arjay@verticalops.com>
2015-09-15 23:12:36 +0800

    update dependency

--- resources/views/patient/index.blade.php
+++ resources/views/patient/index.blade.php
@@ -24,6 +24,7 @@
 
 
 @section('javascript')
+    <script type="text/javascript" src="/js/app/directives/form-error.js"></script>
     <script type="text/javascript" src="/js/app/modules/manage-patient.js"></script>
     <script type="text/javascript" src="/js/ui-grid/ui-grid.min.js"></script>
 
