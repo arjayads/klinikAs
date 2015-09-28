f6e2ede9e77f4ca5368ee29b332a17f772bbcc1b
Arjay Ads <arjay@verticalops.com>
2015-09-29 06:21:52 +0800

    update nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -18,20 +18,6 @@
                 <a href="/medicine">Medicines</a>
             </li>
             <!-- End Medicines -->
-
-            <!-- Search Block -->
-            <li>
-                <i class="search fa fa-search search-btn"></i>
-                <div class="search-open">
-                    <div class="input-group animated fadeInDown">
-                        <input type="text" class="form-control" placeholder="Search">
-                                <span class="input-group-btn">
-                                    <button class="btn-u" type="button">Go</button>
-                                </span>
-                    </div>
-                </div>
-            </li>
-            <!-- End Search Block -->
         </ul>
     </div><!--/end container-->
 </div><!--/navbar-collapse-->
