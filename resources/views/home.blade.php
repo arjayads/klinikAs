352ecfdb3fa9f617a130207cae4412f3a582de56
Arjay Ads <arjay@verticalops.com>
2015-10-01 00:26:58 +0800

    implement unify on authenticated pages

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.unify')
+
+@section('title', 'Patients')
+
+@section('css')
+@stop
+
+@section('content')
+    <h2 class="headline">Welcome Home!</h2>
+@stop
+
+
+@section('javascript')
+@stop
